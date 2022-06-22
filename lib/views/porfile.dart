c64ac777c2e0d96a70583bbfe6a203ccf290cefa
Luis Payares Joly <lpayares@lsv-tech.com>
2022-06-21 22:46:00 -0500

    Profile, FireBase Registers

--- lib/views/porfile.dart
+++ lib/views/porfile.dart
@@ -9,35 +9,111 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    
+    final double coverHeight = 180;
+    final double profileHeight = 144;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            CircleAvatar(
-                backgroundImage: AssetImage('assets/images/Erick.jpeg'),
-                radius: 130),
-            Text(
-              "\n Name: Erick Contreras",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              "Username: Erikbk",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              "Cel: 3053715327",
-              style: TextStyle(fontSize: 20),
-            ),
-            Text(
-              "Email: Erick.work@outlook.es",
-              style: TextStyle(fontSize: 20),
-            ),
+      
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          buildTop(coverHeight, profileHeight),
+          buildName()
+          ]
+          ),
+      /* body: Center(
+        child : Column(
+          children:  const[
+            Text("\n Luis Payares Joly", style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500),),
+            CircleAvatar( 
+              backgroundImage: AssetImage('assets/images/LuisPayares.jpeg'),radius: 130),
+              Text("\n Username: luispayaresj",style: TextStyle(fontSize: 20),),
+              Text("\n Cel: 3214567899",style: TextStyle(fontSize: 20),),
+              Text("\n Email: lpayares@lsv-tech.com",style: TextStyle(fontSize: 20),),
           ],
-        ),
-      ),
+          
+          )
+      ) */
     );
   }
+
+  Widget buildName() => Center(
+
+    child: Card(
+      
+      elevation:2,
+      child: SizedBox(
+        width: 350,
+        
+        child: Column(
+          children: const  [
+            Text('\n User Data', style: TextStyle(fontWeight: FontWeight.bold, fontSize:24)),
+            Text('\n Name:  Daniel', style: TextStyle( color: Colors.blueGrey,fontSize:20)),
+            Text('\n Last Name : Monsalve Jaraba', style: TextStyle(color:Colors.blueGrey, fontSize:20)),
+            Text('\n e-Mail : daniel@gmail.com', style: TextStyle(color:Colors.blueGrey, fontSize:20)),
+            Padding(
+              padding: EdgeInsets.all(14.0),
+              child: Text('\n Telephone : 3114587899', style: TextStyle(color:Colors.blueGrey, fontSize:20)),
+            ),
+            
+    ]
+        ),
+      )
+    )
+    
+    
+    ,);
+
+    Widget buildTop(coverHeight, profileHeight){
+
+      final top = coverHeight - profileHeight/2;
+      final bottom = profileHeight/2;
+      return Stack(
+
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          Container(
+            child: buildCoverImage(coverHeight),
+            margin: EdgeInsets.only(bottom:bottom),
+            ),
+          Positioned(
+            top:top,
+            child: buildProfileImage(profileHeight),
+          ),
+          
+          //const SizedBox(height:24),
+          //buildName(user),
+        ]
+      );
+    }
+
+    Widget buildCoverImage(height) => Container(
+      color:Colors.grey,
+      child: Image.network(
+        'https://www.aoe.com/fileadmin/AOE.com/Images/main_navigation/insights/Fotolia_58569245_Software_Development_930_590_70.jpg',
+        width: double.infinity,
+        height: height ,
+        fit: BoxFit.cover
+        )
+    );
+
+    
+
+    Widget buildProfileImage(height) => CircleAvatar(
+
+      backgroundColor: Colors.white,
+      
+      radius: height/2,
+      foregroundColor: Colors.grey,
+      child: const CircleAvatar(
+      backgroundImage: NetworkImage('https://mysuenos.com/wp-content/uploads/2019/10/joven.jpg'),
+      radius: 65,
+    ),
+
+    );
 }
+
+
