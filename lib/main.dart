f835a6c64525e2034488cdd9542b927329c1e9df
Erick <econtreras@lsv-tech.com>
2022-06-22 17:02:42 -0500

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/views/getfirebase.dart';
-import 'package:flutter_application_2/views/listview_1.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter_application_2/views/splash.dart';
 
 void main() async {
